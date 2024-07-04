6330f75e0938e47ef5b6d2d286bf478ec269323d
abhishek-ajithkumar <abhishek.kumar@jobinandjismi.com>
2024-07-04 09:54:46 +0530

    OTP-7450-CI-Script Updated

--- OTP-7283/src/FileCabinet/SuiteScripts/JobinAndJismi/jj_ue_otp_7450.js
+++ OTP-7283/src/FileCabinet/SuiteScripts/JobinAndJismi/jj_ue_otp_7450.js
@@ -2,6 +2,30 @@
  * @NApiVersion 2.1
  * @NScriptType UserEventScript
  */
+/**********************************************************************************
+ * OTP-7450 : Identify change in address
+ *
+ *
+ * ********************************************************************************
+ *
+ * ********************
+ * company name
+ *
+ * Author: Jobin and Jismi IT Services
+ *
+ *
+ * Date Created: 03-July-2024
+ *
+ * Description: This script is to check a custom checkbox only if there is a change in the existing Address or new Address is added to the Customer Record.
+ *
+ *
+ * REVISION HISTORY
+ *
+ * @version 1.0 company name: 03-July-2024: Created the initial build by JJ0347
+ *
+ *
+ *
+ **************/
 define(['N/record'],
     /**
  * @param{record} record
@@ -30,6 +54,31 @@ define(['N/record'],
          */
         const beforeSubmit = (scriptContext) => {
 
+             try {
+                if (scriptContext.type !== scriptContext.UserEventType.EDIT) {
+                    return;
+                }
+                else {
+ 
+                    let newRecord = scriptContext.newRecord;
+                    let oldRecord = scriptContext.oldRecord;
+ 
+ 
+                    if (addressChanged(newRecord, oldRecord)) {
+                        newRecord.setValue({ fieldId: 'custentity_jj_address_changed', value: true });
+                    } 
+                    else {
+                        newRecord.setValue({ fieldId: 'custentity_jj_address_changed', value: false });
+                    }
+                }
+ 
+            } catch (e) {
+                log.error({
+                    title: 'Error occured',
+                    details: e.toString()
+                });
+            }
+
         }
 
         /**
@@ -42,6 +91,42 @@ define(['N/record'],
          */
         const afterSubmit = (scriptContext) => {
 
+        }
+
+         /** The function addressChanged is used for checking any the address change occured in the customer record or not */
+         function addressChanged(newRecord, oldRecord) {
+            try {
+                let addressChange = false;
+                let newAddressCount = newRecord.getLineCount({ sublistId: 'addressbook' });
+                let oldAddressCount = oldRecord ? oldRecord.getLineCount({ sublistId: 'addressbook' }) : 0;
+                if (newAddressCount !== oldAddressCount) {
+                    addressChange = true;
+                }
+                else {
+                    for (let i = 0; i < newAddressCount; i++) {
+                        let fields = newRecord.getSublistFields({ sublistId: 'addressbook', line: i });
+                        for (let j = 0; j < fields.length; j++) {
+                            let fieldId = fields[j];
+                            let newValue = newRecord.getSublistValue({ sublistId: 'addressbook', fieldId: fieldId, line: i });
+                            let oldValue = oldRecord.getSublistValue({ sublistId: 'addressbook', fieldId: fieldId, line: i });
+                            if (newValue !== oldValue) {
+                                addressChange = true;
+                                break;
+                            }
+                        }
+                        if (addressChange) {
+                            break;
+                        }
+                    }
+                }
+                return addressChange;
+            } catch (e) {
+                log.error({
+                    title: 'Error occured in addressChanged function',
+                    details: e.toString()
+                });
+                return false;
+            }
         }
 
         return {beforeLoad, beforeSubmit, afterSubmit}
